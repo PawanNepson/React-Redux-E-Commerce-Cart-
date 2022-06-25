1c11b4b6a8c7b31848b082983f97f6080e12a81c
Pawan <nepsonh@gmail.com>
2022-06-25 15:54:27 +0545

    header made using font awesome material ui and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,27 @@
-import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Badge from "@mui/material/Badge";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Add to Cart</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+          </Nav>
+          <Badge badgeContent={4} color="primary">
+            <i
+              class="fa-solid fa-cart-shopping text-light"
+              style={{ fontSize: 25, cursor: "pointer" }}
+            ></i>
+          </Badge>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
 export default Header;
