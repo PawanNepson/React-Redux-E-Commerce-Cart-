de363bad9837076a7cbe8bd5627698d35daa1502
Pawan <nepsonh@gmail.com>
2022-06-26 12:08:22 +0545

    card component added from bootstrap

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,24 @@
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 function Cards() {
-  return <div>Cards</div>;
+  return (
+    <div className="container mt-3  ">
+      <h2 className="text-center">Nepson Mart🛒</h2>
+      <div className="row">
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  );
 }
 
 export default Cards;
