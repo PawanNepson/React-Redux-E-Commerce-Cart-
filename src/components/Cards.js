99060d0e33e4e7ff8e8127b15ff93ac07ce3d394
Pawan <nepsonh@gmail.com>
2022-06-26 12:15:30 +0545

    Cards Data is imported and shown in card

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,21 +1,31 @@
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import Cardsdata from "./CardsData";
+import { useState } from "react";
 function Cards() {
+  const [data, setData] = useState(Cardsdata);
+  // console.log(data);
   return (
     <div className="container mt-3  ">
       <h2 className="text-center">Nepson Mart🛒</h2>
       <div className="row">
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
+        {data.map((element, id) => {
+          return (
+            <>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </>
+          );
+        })}
       </div>
     </div>
   );
